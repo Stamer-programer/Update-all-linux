006aa8543a2e7404e7f42281f1fd62baea46a30e
Stamer <stamer123rr@gmail.com>
2025-11-08 23:31:20 +0100

    first commit

---
+++ Update-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "!!Welcome to Linux update!!"
+if ! command -v apt &>/dev/null; then
+  sudo apt update
+  sudo apt upgrade -y
+  sudo apt full-upgrade
+fi
+if ! command -v apt-get &>/dev/null; then
+  echo "<the_command> could not be found"
+fi
+if ! command -v pacman &>/dev/null; then
+  sudp pacman -Syu
+fi
+
+flatpak update
