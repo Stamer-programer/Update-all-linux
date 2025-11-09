c21ccaff6f8ac2be17e1edb2a4939f67b6adeda2
Stamer <stamer123rr@gmail.com>
2025-11-09 01:25:48 +0100

    Enhance Update-all.sh with completion messages for package updates

--- src/Update-all.sh
+++ src/Update-all.sh
@@ -5,13 +5,19 @@ if command -v apt &>/dev/null; then
   sudo apt update
   sudo apt upgrade -y
   sudo apt full-upgrade
+  echo "Apt update complete"
 fi
 if command -v apt-get &>/dev/null; then
-  echo "<the_command> could not be found"
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    sudo apt-get dist-upgrade
+    echo "Apt-get update complete"
 fi
 if command -v pacman &>/dev/null; then
   sudo pacman -Syu
+  echo "Pacman update complete"
 fi
 if command -v flatpak &>/dev/null; then
-flatpak update
+    flatpak update
+    echo "Flatpak update complete"
 fi
