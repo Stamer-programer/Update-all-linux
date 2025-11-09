9b1f5638e34dcddf54bccf0029d755ee1473eedf
Stamer-programer <stamer123rr@gmail.com>
2025-11-09 01:21:58 +0100

    Create Update-all.sh for system updates
    Add a script to update system packages for various package managers.

--- src/Update-all.sh
+++ src/Update-all.sh
@@ -1,1 +1,17 @@
-/home/misha/Documents/Code/sh/update-all/Update-all.sh
+#!/bin/bash
+
+echo "!!Welcome to Linux update!!"
+if command -v apt &>/dev/null; then
+  sudo apt update
+  sudo apt upgrade -y
+  sudo apt full-upgrade
+fi
+if command -v apt-get &>/dev/null; then
+  echo "<the_command> could not be found"
+fi
+if command -v pacman &>/dev/null; then
+  sudo pacman -Syu
+fi
+if command -v flatpak &>/dev/null; then
+flatpak update
+fi
